e565699dc82444ed50e4cdd8b7e8c106c1cedcb7
BZLZHH <admin@bzlzhh.top>
2025-08-18 21:42:16 +0800

    Revert "[Chore] (Version): Bump version to Release 0.3.0"
    This reverts commit a0409be80a3240ce0221f9270e79235ebce4378f.

--- version.h
+++ version.h
@@ -4,8 +4,8 @@
 #define MAJOR 0
 #define MINOR 3
 #define REVISION 0
-#define VERSION_TYPE "Release "
+#define VERSION_TYPE "Development "
 #define VERSION_SUFFIX ""
-#define RELEASE 1
+#define RELEASE 0
 
 #endif //_GL4ES_VERSION_H
